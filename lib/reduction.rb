9c991ac49e7fcac2b907c714b7a8f62f370b2800
Jeff Pollard <jeff.pollard@gmail.com>
2011-05-01 21:15:40 -0700

    Stub for reduction spec

--- lib/reduction.rb
+++ lib/reduction.rb
@@ -1,3 +1,11 @@
 module Reduction
-  # Your code goes here...
+  ROOT = Pathname.new File.expand_path('./..', File.dirname(__FILE__))
 end
+
+require 'nokogiri'
+
+require 'core_ext/string'
+
+require 'reduction/strategy'
+require 'reduction/strategy/all_recipes'
+require 'reduction/version'
--- spec/lib/reduction_spec.rb
+++ spec/lib/reduction_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Reduction do
-
+  describe '#strategies' do
+  end
 end
