95ea81fd3d5f186cc96059df5eb575813e451c46
Jeff Pollard <jeff.pollard@gmail.com>
2011-05-15 21:28:15 -0700

    Add NamedList.from_node_set

--- lib/reduction/named_list.rb
+++ lib/reduction/named_list.rb
@@ -1,5 +1,26 @@
 module Reduction
+
   class NamedList < Array
     attr_accessor :name
+
+    def self.from_node_set(nodeset, title_elem)
+      stack = Array.new
+
+      nodeset.each do |elem|
+        case elem.name.to_sym
+        when title_elem.to_sym
+          stack.push(NamedList.new.tap { |l| l.name = elem.text })
+        when :ul, :ol
+          if list = stack.pop
+            ingredient_list = elem.search('li').map(&:text)
+            ingredient_list.clean!
+            stack.push(list.replace(ingredient_list))
+          end
+        end
+      end
+
+      stack
+    end
   end
+
 end
--- spec/lib/reduction/named_list_spec.rb
+++ spec/lib/reduction/named_list_spec.rb
@@ -16,6 +16,47 @@ module Reduction
       subject.name.should == 'Steve'
     end
 
+    describe '.from_node_set' do
+
+      let(:doc) do
+        <<-EOF
+        <div>
+          <h4>Title 1</h4>
+          <ul><li>Item 1</li><li>Item 2</li></ul>
+          <h4>Title 2</h4>
+          <ul><li>Item 3</li><li>Item 4</li></ul>
+        </div>
+        EOF
+      end
+
+      let(:nodeset) { Nokogiri::XML(doc).at('div').children }
+
+      subject { described_class.from_node_set(nodeset, :h4) }
+
+      it 'returns an array of NamedList documents' do
+        subject.each do |item|
+          item.should be_a(NamedList)
+        end
+      end
+
+      it 'takes the every other element, starting with the first, as the name of the list' do
+        subject.map(&:name).should == ['Title 1', 'Title 2']
+      end
+
+      it 'takes every other item, starting with the 2nd, as the list' do
+        subject[0].should == ['Item 1', 'Item 2']
+        subject[1].should == ['Item 3', 'Item 4']
+      end
+
+      it 'allows you to use a different title element name' do
+        nodeset.search('h4').each { |e| e.name = 'h5' }
+        described_class.from_node_set(nodeset, :h4).should be_empty
+
+        list = described_class.from_node_set(nodeset, :h5)
+        list.map(&:name).should == ['Title 1', 'Title 2']
+      end
+    end
+
   end
 
 end
