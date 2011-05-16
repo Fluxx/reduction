2743ed8434506aaae4800985a7bb9662b2a07788
Jeff Pollard <jeff.pollard@gmail.com>
2011-05-15 20:33:13 -0700

    Handle multi-part ingredient lists for chow

--- lib/reduction/strategy/chow.rb
+++ lib/reduction/strategy/chow.rb
@@ -12,7 +12,11 @@ module Reduction
       end
 
       def ingredients
-        doc.at('#ingredients ul').text.stripped_lines
+        if ingredient_elements.search('h4').any?
+          multi_part_ingredients_section
+        else
+          single_ingredients_section
+        end
       end
 
       def steps
@@ -31,6 +35,34 @@ module Reduction
 
       def total_time
         doc.at('#servings p[2] time[itemprop=totalTime]').text.collapse_whitespace
+      end
+
+      private
+
+      def ingredient_elements
+        @ingredient_elements ||= doc.at('#ingredients').children
+      end
+
+      def multi_part_ingredients_section
+        stack = Array.new
+
+        ingredient_elements.each do |elem|
+          case elem.name.to_sym
+          when :h4
+            stack.push(NamedList.new.tap { |l| l.name = elem.text })
+          when :ul
+            list = stack.pop
+            ingredient_list = elem.search('li').map(&:text)
+            ingredient_list.clean!
+            stack.push(list.replace(ingredient_list))
+          end
+        end
+
+        stack
+      end
+
+      def single_ingredients_section
+        ingredient_elements.search('li').map(&:text).map(&:collapse_whitespace)
       end
 
     end
