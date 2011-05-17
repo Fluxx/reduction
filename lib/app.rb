d86b8f88e440e4ab8bd345c3344379dae51672a8
Jeff Pollard <jeff.pollard@gmail.com>
2011-05-16 22:03:06 -0700

    Ghetto rack app

---
+++ lib/app.rb
@@ -0,0 +1,50 @@
+require 'rubygems'
+require 'sinatra'
+require 'open-uri'
+
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__)))
+
+require 'reduction'
+
+def form
+  %Q{<form method="post"><input type="text" name="url" size="100"  value="#{params['url']}"> \
+     </input><input type="submit" value="Submit"></form>}
+end
+
+def reduction(url)
+  Reduction::Strategy.constants.each do |klass|
+    strategy = Reduction::Strategy.const_get(klass)
+    if strategy.for_url?(url)
+      return strategy.new(open(url).read)
+    end
+  end
+  
+  ''
+end
+
+def reduce(url)
+  reduc = reduction(url)
+  
+  "<table border=1>".tap do |buffer|
+    reduc.methods.first(6).each do |meth|
+      buffer << "<tr><td><strong>#{meth}</strong></td><td>"
+      result = reduc.send(meth)
+      buffer << (result.is_a?(Array) ? '<ol>' + result.map { |e| "<li>#{e}</li>" }.join + '</ol>': result)
+      buffer << "</td></tr>"
+    end
+    
+    buffer << "</table>"
+  end
+end
+
+def iframe(url)
+  "<iframe src=\"#{url}\" width=\"100%\" height=\"500\"></iframe>"
+end
+
+get '/' do
+  form
+end
+
+post '/' do
+  [Reduction::Strategy.constants.inspect, form, reduce(params['url']), iframe(params['url'])].join('<hr>')
+end
