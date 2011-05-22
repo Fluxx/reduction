d0de80d83415156ae081a862f8c0edcce9a86173
Jeff Pollard <jeff.pollard@gmail.com>
2011-05-21 18:23:45 -0700

    Re-record interval for VCR

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -3,5 +3,6 @@ require 'vcr'
 VCR.config do |c|
   c.cassette_library_dir = 'spec/vcr_cassettes'
   c.stub_with :fakeweb
-  c.default_cassette_options = { :record => :new_episodes }
+  # Record every 2 days
+  c.default_cassette_options = { :record => :new_episodes, :re_record_interval => 60*60*12 }
 end
