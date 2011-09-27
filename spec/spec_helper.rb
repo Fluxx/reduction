7a8f78530e02ced7e0c2847c68cadaac17bb9a8e
Jeff Pollard <jeff.pollard@gmail.com>
2011-09-26 22:51:29 -0700

    Use the symbol metadata keys feature of rspec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,4 +13,5 @@ RSpec.configure do |config|
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
   config.alias_it_should_behave_like_to :it_should_find, "it should find a"
+  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
