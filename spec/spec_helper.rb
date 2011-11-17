f8fdd25c97b2b8a8757aef552fda35206d8d06d4
Jeff Pollard <jeff.pollard@gmail.com>
2011-11-16 23:09:14 -0800

    Add macro to set the subject for a spec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,12 @@ require 'open-uri'
 
 Dir['./spec/support/**/*.rb'].each { |f| require f }
 
+module StrategyMacros
+  def strategy_subject_for(url)
+    subject { described_class.new(get_page(url), url) }
+  end
+end
+
 RSpec.configure do |config|
   config.color_enabled = true
   config.debug = true
@@ -14,4 +20,6 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.alias_it_should_behave_like_to :it_should_find, "it should find a"
   config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  config.extend StrategyMacros
 end
